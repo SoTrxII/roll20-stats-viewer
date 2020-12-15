7feb313e76878d701ba12b79c47f4760daa4b5d0
SoTrxII <SoTrxII@users.noreply.github.com>
2020-12-15 13:59:57 +0000

    deploy: 9ac3018e0be4d3dcc5fb1e2874b5a95d8812b2bc

---
+++ precache-manifest.26f0a9e5e7fa35ebbadfd53b481256d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e03ae56e49b24c1d626",
+    "url": "/css/app.321af31d.css"
+  },
+  {
+    "revision": "05110edb7314878f515e",
+    "url": "/css/chunk-vendors.1875260c.css"
+  },
+  {
+    "revision": "24e5d737d24162fabbe82fdcd5fab3ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e03ae56e49b24c1d626",
+    "url": "/js/app.c6c3faf1.js"
+  },
+  {
+    "revision": "05110edb7314878f515e",
+    "url": "/js/chunk-vendors.f3d72a36.js"
+  },
+  {
+    "revision": "53de22600e0cc0ecf6457b0cd1792d00",
+    "url": "/manifest.json"
+  }
+]);
